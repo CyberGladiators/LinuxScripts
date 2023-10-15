713a721d100051fcd2ed4aef52d472d1d1215da4
SpainWithoughtTheS <103275152+SpainWithoughtTheS@users.noreply.github.com>
2023-10-15 15:53:30 -0500

    Update unauthusers.py

--- src/unauthusers.py
+++ src/unauthusers.py
@@ -34,7 +34,7 @@ def check_unauthorized_users():
     system_users = ['root', 'daemon', 'bin', 'sys', 'sync', 'games', 'man', 'lp', 'mail',
                     'news', 'uucp', 'proxy', 'www-data', 'backup', 'list', 'irc',
                     'gnats', 'nobody', 'systemd-network', 'systemd-resolve', 'syslog',
-                    'messagebus', '_apt', 'tss', 'sshd', 'landscape', 'pollinate', 'colord, whoopsie']
+                    'messagebus', '_apt', 'tss', 'sshd', 'landscape', 'pollinate', 'colord', 'whoopsie']
 
     check_users(allowed_users, system_users)
 
