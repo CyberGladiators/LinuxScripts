ffd77db6bd7f8664569aed7ac8ab73215fe7c04f
TheRealDownloadTrump <128330477+TheRealDownloadTrump@users.noreply.github.com>
2023-10-15 21:22:47 +0000

    Update minlenForDebian.py

--- src/minlenForDebian.py
+++ src/minlenForDebian.py
@@ -1,20 +1,45 @@
 import subprocess
 
 
-subprocess.run(["sudo", "cp", "/etc/pam.d/common-password", "/etc/pam.d/common-password.bak"], check=True)
+def is_minlen_set(line):
+
+    params = line.split()
+    for param in params:
+        if param.startswith("minlen="):
+            length = param.split("=")[1]
+            if length.isdigit() and int(length) >= 8:
+                return True
+    return False
 
 def passwdMinimumForDebian():
     try:
-        with open("/etc/pam.d/common-password", "r") as f:
-            lines = f.readlines()
 
-        with open("/etc/pam.d/common-password", "w") as f:
+        subprocess.run(["sudo", "cp", "/etc/pam.d/common-password", "/etc/pam.d/common-password.bak"], check=True)
+
+        with open("/etc/pam.d/common-password", "r") as file:
+            lines = file.readlines()
+
+
+        modified = False
+
+        with open("/etc/pam.d/common-password", "w") as file:
             for line in lines:
-                if "pam_unix.so" in line:
-                    f.write(line.strip() + " minlen=8\n")
-                else:
-                    f.write(line)
+                if "pam_unix.so" in line and not is_minlen_set(line):
 
-        print("Password length requirement has been set.")
+                    file.write(line.strip() + " minlen=8\n")
+                    modified = True
+                else:
+                    file.write(line)
+
+        if modified:
+            print("Password length requirement has been set.")
+        else:
+            print("Password length requirement was already set.")
+
     except PermissionError:
         print("You must run this script with sudo permissions.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+
+passwdMinimumForDebian()
