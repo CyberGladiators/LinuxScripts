507698fc366cb74d94bb6c6e39d4704899aefd95
TheRealDownloadTrump <128330477+TheRealDownloadTrump@users.noreply.github.com>
2023-10-15 04:30:49 +0000

    Update remove.py

--- src/remove.py
+++ src/remove.py
@@ -16,7 +16,19 @@ def remove_package(package):
         print(f"An error occurred: {str(e)}")
 
 def remove_packages():
-    packages = ["nmap", "metasploit-framework", "burpsuite", "john", "hydra", "sqlmap", "wireshark", "aircrack-ng", "nikto", "gobuster", "dirb", "hashcat", "wpscan", "enum4linux", "netcat", "openvpn", "ophcrack"]
+    packages = [
+    "nmap", "metasploit-framework", "burpsuite", "john", "hydra", "sqlmap", "wireshark", 
+    "aircrack-ng", "nikto", "gobuster", "dirb", "hashcat", "wpscan", "enum4linux", "netcat", 
+    "openvpn", "ophcrack", "binwalk", "radare2", "masscan", "snort", "maltego", "cewl", 
+    "recon-ng", "theharvester", "dnsrecon", "beef-xss", "cisco-torch", "etherape", "yersinia", 
+    "zaproxy", "kismet", "hping3", "armitage", "netsniff-ng", "cuckoo", "volatility", "mimikatz",
+    "setoolkit", "msfpc", "uniscan", "veil", "wifite", "responder", "empire", "mitmproxy",
+    "patator", "medusa", "fcrackzip", "foremost", "sleuthkit", "autopsy", "chntpw", "flasm", 
+    "macchanger", "truecrack", "tcpflow", "tcpreplay", "tcpdump", "sslyze", "powersploit",
+    "reaver", "pixiewps", "photon", "lynis", "joomscan", "ettercap", "sslstrip", "slowhttptest",
+    "skipfish", "seclists", "pyrit", "proxychains", "proxychains-ng"
+]
+
     for package in packages:
         if is_package_installed(package):
             answer = input(f"{package} is installed. Do you want to remove it? (y/n): ")
