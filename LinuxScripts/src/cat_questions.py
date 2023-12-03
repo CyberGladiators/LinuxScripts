8992064288f474b9315570d970853826109e37cc
TheRealDownloadTrump <128330477+TheRealDownloadTrump@users.noreply.github.com>
2023-12-03 13:45:03 -0600

    Add files via upload

---
+++ LinuxScripts/src/cat_questions.py
@@ -0,0 +1,25 @@
+import os
+import glob
+
+def search_and_display_files():
+    # Change '~/Desktop' to the full path if necessary
+    directory = os.path.expanduser('~/Desktop')
+    pattern = 'Forensics Questions_*'
+
+    # Search for files that match the pattern
+    files = glob.glob(os.path.join(directory, pattern))
+
+    # Display file contents
+    for file in files:
+        if os.path.isfile(file):
+            print(f"Displaying contents of {file}:")
+            with open(file, 'r') as f:
+                print(f.read())
+            print("-----\n")
+    else: 
+        print(" no Forensics_Questions found, something is probably wrong edit the cat_questions.py file to fix ")
+
+# Call the function
+
+if __name__ == "__main__":
+    search_and_display_files()
---
+++ LinuxScripts/src/main.py
@@ -0,0 +1,73 @@
+import subprocess
+import os
+import pwd
+import glob
+
+from find import search_files  
+from ssh_config import modify_ssh_config  
+from minlen import passwdMinimum
+from crontab_find import list_crontabs
+from cat_questions import search_and_display_files
+from lightdm_conf import lightdm_configure
+from update import update_system
+from remove import remove_packages
+from unauthusers import check_unauthorized_users
+from changepasswd import change_passwords
+from minlenForDebian import passwdMinimumForDebian
+
+def display_ascii_art(file_path):
+    with open(file_path, 'r') as file:
+        ascii_art = file.read()
+    print(ascii_art)
+
+display_ascii_art('ascii_art.txt')
+
+def main_options():
+    try:
+        print("Choose the main option you want to execute:")
+        print("1. Search for files")
+        print("2. Modify SSH Config")
+        print("3. add minimum passwd of 8")
+        print("4. remove packages")
+        print("5. list all the Forensics_Questions")
+        print("6. find all users crontabs")
+        print("7. Lightdm configure")
+        print("8. update the system ")
+        print("9. list unauth users")
+        print("10. change all user passwords")
+        print("11. minlen for debian it uses pam btw")
+
+
+        choice = input("$ ")
+
+        if choice == '1':
+            search_files()
+        elif choice == '2':
+            modify_ssh_config()
+        elif choice == '3':
+            passwdMinimum()
+        elif choice == '4':
+             remove_packages()
+        elif choice == '5':
+            search_and_display_files()
+        elif choice == '6':
+            list_crontabs()
+        elif choice == '7':
+            lightdm_configure()  
+        elif choice == '8':
+            update_system()  
+        elif choice == '9':
+            check_unauthorized_users()  
+        elif choice == '10':
+            change_passwords()
+        elif choice == '11':
+            passwdMinimumForDebian()
+        else:
+            print("Invalid choice")
+
+
+    except KeyboardInterrupt:
+        print("\nProgram interrupted. Exiting gracefully.")
+        exit(0)
+if __name__ == "__main__":
+    main_options()
