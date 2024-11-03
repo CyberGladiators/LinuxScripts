3dca65a324586ae859a0d98c02aff23403b1e8ac
TheRealDownloadTrump <128330477+TheRealDownloadTrump@users.noreply.github.com>
2024-11-03 16:51:14 -0600

    round 2 script update 1.3

--- src/common_auth_nullok.py
+++ src/common_auth_nullok.py
@@ -1,16 +1,16 @@
 def remove_nullok_option():
     config_file = "/etc/pam.d/common-auth"
-    search_line = "auth [success=2 default=ignore] pam_unix.so"
+    search_keyword = "pam_unix.so"
     nullok_option = "nullok"
 
     # Read the contents of the file
     with open(config_file, 'r') as file:
         lines = file.readlines()
 
-    # Modify the line if it contains the search_line
+    # Modify the line if it contains the search_keyword and nullok_option
     modified = False
     for i, line in enumerate(lines):
-        if search_line in line and nullok_option in line:
+        if search_keyword in line and nullok_option in line:
             # Remove the 'nullok' option
             lines[i] = line.replace(nullok_option, "").strip() + "\n"
             modified = True
@@ -24,7 +24,5 @@ def remove_nullok_option():
     else:
         print("'nullok' option already removed or line not found.")
 
-
-# Usage in main.py
 if __name__ == "__main__":
     remove_nullok_option()
